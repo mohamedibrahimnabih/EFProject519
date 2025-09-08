a567f362919f8c9e1715ef1d8516789aafdec110
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-09-08 22:02:58 +0300

    Add Age Prop To Employee Model

--- SolveChallenge/Models/Employee.cs
+++ SolveChallenge/Models/Employee.cs
@@ -6,6 +6,7 @@
         public int DepartmentId { get; set; }
         public string Name { get; set; }
         public decimal Salary { get; set; }
+        public int Age { get; set; }
         public string MainImg { get; set; }
     }
 }
